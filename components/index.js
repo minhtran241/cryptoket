c209c5ee90891ea07a3e5216d3b8d6adde487d46
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-12-14 11:51:33 -0500

    add NFTDetails page & Checkout Modal without Checkout function

--- components/index.js
+++ components/index.js
@@ -6,5 +6,16 @@ import CreatorCard from './CreatorCard';
 import NFTCard from './NFTCard';
 import Input from './Input';
 import Loader from './Loader';
+import Modal from './Modal';
 
-export { Navbar, Footer, Button, Banner, CreatorCard, NFTCard, Input, Loader };
+export {
+  Navbar,
+  Footer,
+  Button,
+  Banner,
+  CreatorCard,
+  NFTCard,
+  Input,
+  Loader,
+  Modal,
+};
