176a21bc570262ebcbf0b1d05d52ad9c12af9186
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-12-14 22:09:07 -0500

    🔍 add SearchBar component: Debounced Search & Filter

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -4,22 +4,86 @@ import { useTheme } from 'next-themes';
 
 import images from '../assets';
 
-const SearchBar = () => {
+const SearchBar = ({
+  activeSelect,
+  setActiveSelect,
+  handleSearch,
+  clearSearch,
+}) => {
   const [search, setSearch] = useState('');
+  const [debouncedSearch, setDebouncedSearch] = useState(search);
+  const [toggle, setToggle] = useState(false);
   const { theme } = useTheme();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearch(debouncedSearch);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [debouncedSearch]);
+
+  useEffect(() => {
+    if (search) {
+      handleSearch(search);
+    } else {
+      clearSearch();
+    }
+  }, [search]);
 
   return (
     <>
-      <div className="flex-1 flexCenter dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 px-4 rounded-md">
+      <div className="flex-1 flexCenter dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md">
         <Image
           src={images.search}
           objectFit="contain"
           width={20}
           height={20}
           alt="search"
-          className={theme === 'light' && 'filter invert'}
+          className={theme === 'light' ? 'filter invert' : undefined}
         />
-        <input type="text" placeholder="Search for NFT..." className="dark:bg-nft-black-2 bg-white mx-4 w-full dark:text-white text-nft-black-1 font-normal text-xs outline-none" onChange={() => {}} value="" />
+        <input
+          type="text"
+          placeholder="Search item here"
+          className="dark:bg-nft-black-2 bg-white mx-4 w-full font-poppins dark:text-white text-nft-black-1 font-normal text-xs outline-none"
+          onChange={(e) => setDebouncedSearch(e.target.value)}
+          value={debouncedSearch}
+        />
+      </div>
+
+      <div
+        onClick={() => setToggle((prevToggle) => !prevToggle)}
+        className="relative flexBetween ml-4 sm:ml-0 sm:mt-2 min-w-190 cursor-pointer dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md"
+      >
+        <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs">
+          {activeSelect}
+        </p>
+        <Image
+          src={images.arrow}
+          objectFit="contain"
+          width={15}
+          height={15}
+          alt="arrow"
+          className={theme === 'light' ? 'filter invert' : undefined}
+        />
+
+        {toggle && (
+          <div className="absolute top-full left-0 right-0 w-full mt-3 z-10 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md">
+            {[
+              'Recently added',
+              'Price (low to high)',
+              'Price (high to low)',
+            ].map((item) => (
+              <p
+                className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs my-3 cursor-pointer"
+                onClick={() => setActiveSelect(item)}
+                key={item}
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -8,15 +8,36 @@ import { shortenAddress } from '../utils/shortenAddress';
 
 const MyNFTs = () => {
   const [nfts, setNfts] = useState([]);
+  const [nftsCopy, setNftsCopy] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { fetchMyNFTsOrListedNFTs, currentAccount } = useContext(NFTContext);
+  const [activeSelect, setActiveSelect] = useState('Recently Added');
 
   useEffect(() => {
     fetchMyNFTsOrListedNFTs('fetchMyNFTs').then((items) => {
       setNfts(items);
+      setNftsCopy(items);
       setIsLoading(false);
     });
   }, []);
+
+  useEffect(() => {
+    const sortedNfts = [...nfts];
+    switch (activeSelect) {
+      case 'Price (low to high)':
+        setNfts(sortedNfts.sort((a, b) => a.price - b.price));
+        break;
+      case 'Price (high to low)':
+        setNfts(sortedNfts.sort((a, b) => b.price - a.price));
+        break;
+      case 'Recently added':
+        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+        break;
+      default:
+        setNfts(nfts);
+        break;
+    }
+  }, [activeSelect]);
 
   if (isLoading) {
     return (
@@ -25,6 +46,24 @@ const MyNFTs = () => {
       </div>
     );
   }
+
+  const onHandleSearch = (value) => {
+    const filteredNfts = nfts.filter(({ name }) =>
+      name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredNfts.length) {
+      setNfts(filteredNfts);
+    } else {
+      setNfts(nftsCopy);
+    }
+  };
+
+  const onClearSearch = () => {
+    if (nfts.length && nftsCopy.length) {
+      setNfts(nftsCopy);
+    }
+  };
 
   return (
     <div className="w-full flex justify-start items-center flex-col min-h-screen">
@@ -47,7 +86,7 @@ const MyNFTs = () => {
           </p>
         </div>
       </div>
-      {!isLoading && nfts.length === 0 ? (
+      {!isLoading && !nfts.length && !nftsCopy.length ? (
         <div className="flexCenter sm:p-4 p-16">
           <h1 className="font-poppins dark:text-white text-nft-black-1 text-3xl font-normal">
             No NFT Owned
@@ -56,7 +95,12 @@ const MyNFTs = () => {
       ) : (
         <div className="sm:px-4 p-12 w-full minmd:w-4/5 flexCenter flex-col">
           <div className="flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8">
-            <SearchBar />
+            <SearchBar
+              activeSelect={activeSelect}
+              setActiveSelect={setActiveSelect}
+              handleSearch={onHandleSearch}
+              clearSearch={onClearSearch}
+            />
           </div>
           <div className="mt-3 w-full flex flex-wrap">
             {nfts.map((nft) => (
